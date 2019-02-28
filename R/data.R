6bfe3624ebf0f96b1a46990b56bb8626c85ef3cb
Dan Jordan <dandermotj@gmail.com>
2019-02-28 23:47:10 +0000

    Documentation for data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @importFrom tibble tibble
+NULL
+
+#'  Ainmneacha Naíonán na hÉireann - Irish Baby Names (1964 - 2018)
+#'
+#' Irish baby name data provided by the Irish CSO This includes all names
+#' that occur at least three times in the year.
+#'
+#' @format A data frame with five variables: `year`, `sex`, `name` and `n`
+"irishbabynames"

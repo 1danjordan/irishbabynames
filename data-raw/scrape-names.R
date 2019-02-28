4b9e7c243e630d171d4f864884d171c6f74e47bf
Dan Jordan <dandermotj@gmail.com>
2019-02-28 23:46:28 +0000

    Irish baby name data raw and tidy with script for scraping

---
+++ data-raw/scrape-names.R
@@ -0,0 +1,38 @@
+# Read in PC-AXIS files tidy them, merge them and save as tibble
+library(dplyr)
+
+boys_link <- "https://www.cso.ie/px/pxeirestat/Database/eirestat/Irish%20Babies%20Names/VSA05.px"
+girls_link <- "https://www.cso.ie/px/pxeirestat/Database/eirestat/Irish%20Babies%20Names/VSA12.px"
+
+boys <- pxR::read.px(boys_link) %>%
+  as.data.frame() %>%
+  filter(!grepl("Rank", Statistic)) %>%
+  select(-Statistic) %>%
+  as_tibble() %>%
+  setNames(tolower(names(.))) %>%
+  mutate(
+    year = as.integer(levels(year))[year],
+    name = as.character(name),
+    value = as.integer(value),
+    sex = "M"
+  ) %>%
+  select(year, sex, name, n = value)
+
+girls <- pxR::read.px(girls_link) %>%
+  as.data.frame() %>%
+  filter(!grepl("Rank", Statistic)) %>%
+  select(-Statistic) %>%
+  as_tibble() %>%
+  setNames(tolower(names(.))) %>%
+  mutate(
+    year = as.integer(levels(year))[year],
+    name = as.character(name),
+    value = as.integer(value),
+    sex = "M"
+  ) %>%
+  select(year, sex, name, n = value)
+
+irishbabynames <- bind_rows(boys, girls)
+
+write.csv(irish_baby_names, "data-raw/irish-baby-names.csv")
+usethis::use_data(irishbabynames, compress = "xz", overwrite = TRUE)

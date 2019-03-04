ab79307e1795b7cb3548ca07e0accf26fc1e4ced
Dan Jordan <dandermotj@gmail.com>
2019-03-04 19:19:10 +0000

    Add irishpopulation.Rda and document

--- R/data.R
+++ R/data.R
@@ -1,10 +1,16 @@
 #' @importFrom tibble tibble
 NULL
 
-#'  Ainmneacha Naíonán na hÉireann - Irish Baby Names (1964 - 2018)
+#' Ainmneacha Naíonán na hÉireann - Irish Baby Names (1964 - 2018)
 #'
-#' Irish baby name data provided by the Irish CSO This includes all names
+#' Irish baby name data provided by the Irish CSO. This includes all names
 #' that occur at least three times in the year.
 #'
 #' @format A data frame with five variables: `year`, `sex`, `name` and `n`
 "irishbabynames"
+
+#' Irish populationg statistics by gender (1926 - 2018)
+#'
+#' Statistics collected by the Irish CSO under Annual Population
+#' Estimates PEA11.
+"irishpopulation"

2f560d0c2c6ef0cc373529cb43fb7bad736e293b
Dan Jordan <dandermotj@gmail.com>
2019-03-05 23:28:28 +0000

    Rename irishpopulation dataset to population

--- data-raw/irish-population.R
+++ data-raw/irish-population.R
@@ -2,21 +2,23 @@ library(dplyr)
 
 irish_pop_link <- "https://www.cso.ie/px/pxeirestat/Database/eirestat/Annual%20Population%20Estimates/PEA11.px"
 
-irishpopulation <- pxR::read.px(irish_pop_link) %>%
+population <- pxR::read.px(irish_pop_link) %>%
   as.data.frame() %>%
   as_tibble() %>%
   mutate(age = as.character(Single.Year.of.Age)) %>%
   filter(Sex %in% c("Male", "Female"), age != "All ages") %>%
   transmute(
     year = as.integer(levels(Year))[Year],
-    sex = as.character(Sex),
+    sex = as.character(Sex) %>% tolower(),
     age = case_when(
       age == "Under 1 year" ~ "0 years",
       age == "99 years and over" ~ "99 years",
       TRUE ~ age
-    ) %>% readr::parse_number(),
-    population = value
+    ) %>%
+      readr::parse_number() %>%
+      as.integer(),
+    population = as.integer(value)
   )
 
-readr::write_csv(irishpopulation, "data-raw/irish-population.csv")
-usethis::use_data(irishpopulation, compress = "xz", overwrite = TRUE)
+readr::write_csv(population, "data-raw/population.csv")
+usethis::use_data(population, compress = "xz", overwrite = TRUE)
